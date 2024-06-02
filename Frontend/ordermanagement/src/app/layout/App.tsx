bea8c39d1534128c1f1952ffedc27a4e3ca329f3
Robert Coulston <robert.coulston@gmail.com>
2024-06-02 11:10:22 +1000

    Basic UI nav structure

--- Frontend/ordermanagement/src/app/layout/App.tsx
+++ Frontend/ordermanagement/src/app/layout/App.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import "./styles.css";
 import { ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 import CustomersDashboard from "../../features/customers/customersDashboard/CustomersDashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "../../features/home/HomePage";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "http://localhost:5133/graphql/",
+  uri: process.env.REACT_APP_API_SCHEMA_URL
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage/>} />
+            <Route path="/customers" element={<CustomersDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <CustomersDashboard />
     </ApolloProvider>
   );
