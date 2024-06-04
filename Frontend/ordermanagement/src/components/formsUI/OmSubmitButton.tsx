c09a2523b2c63568e0c2d0cdf571be16e60131f6
Robert Coulston <robert.coulston@gmail.com>
2024-06-04 17:57:39 +1000

    Add submit button

---
+++ Frontend/ordermanagement/src/components/formsUI/OmSubmitButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@mui/material";
+import { useFormikContext } from "formik";
+import React from "react";
+
+type OmSubmitButtonProps = {
+  children: any;
+  otherProps: any;
+};
+
+const OmSubmitButton: React.FC<OmSubmitButtonProps> = ({
+  children,
+  otherProps,
+}) => {
+  const { submitForm } = useFormikContext();
+
+  const handleSubmit = () => {
+    submitForm();
+  };
+
+  const configButton = {
+    ...otherProps,
+    color: "primary",
+    variant: "contained",
+    fullWidth: true,
+    onClick: handleSubmit,
+  };
+
+  return <Button {...configButton}>{children}</Button>;
+};
+
+export default OmSubmitButton;
