0f618bb840a9606eb10b26f93e2e1432e32fe04a
Robert Coulston <robert.coulston@gmail.com>
2024-06-04 17:31:43 +1000

    Dashboards use Alert and Loading reusable components

--- Frontend/ordermanagement/src/features/orders/ordersDashboard/OrdersDashboard.tsx
+++ Frontend/ordermanagement/src/features/orders/ordersDashboard/OrdersDashboard.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Order, useGetOrdersQuery } from '../../../graphql/generated/schema';
 import { Grid, Typography } from '@mui/material';
 import OrderList from '../ordersList/OrderList';
+import OmAlert from '../../../components/elements/OmAlert';
+import OmLoading from '../../../components/elements/OmLoading';
 
 
 export default function OrdersDashboard() {
     const {data: ordersData, loading, error} = useGetOrdersQuery();
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.message}</div>;
+    if (loading) return <OmLoading />;
+    if (error) return <OmAlert message={error.message} />;
     if (!ordersData) return <div>Error with customer data</div>;
 
     const orders = ordersData.orders as Order[];
